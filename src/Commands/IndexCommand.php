1f803f766b61cdac9bdf2b2013a90db56c4e00bc
Levi McGlone <mcglonelevi@gmail.com>
2018-03-27 21:40:43 -0400

    add first command

---
+++ src/Commands/IndexCommand.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../bootstrap.php';
+
+use App\Actions\Todo\IndexAction;
+
+$indexAction = $container->make(IndexAction::class);
+print_r($indexAction->execute());
